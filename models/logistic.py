3c770d4e22ed08cf631a39b33fd109b5a8613726
Andi <dremellyn@gmail.com>
2025-12-01 16:53:52 -0700

    wrote the logistic regression model

---
+++ models/logistic.py
@@ -0,0 +1,66 @@
+"""
+File: logistic.py
+Description: A logistic regression model.
+"""
+
+import numpy as np
+
+
+def sigmoid(z):
+    """
+    sigmoid function -> sigma(z) = 1 / (1 + exp(-z)).
+    :param z: scalar or numpy array of shape (n_samples,).
+    :returns: same shape as z, values in (0,1).
+    """
+    return 1 / (1 + np.exp(-z))
+
+def logistic_loss(w, X, y):
+    """
+    logistic regression loss function.
+    :param w: weight vector, shape (n_features, ).
+    :param X: data matrix, shape (n_samples, n_features).
+    :param y: labels vector, shape (n_samples, ).
+    """
+
+    # linear scores: z_i = w^T x_i
+    # one score per data point
+    z = X @ w               # shape (n_samples, )
+
+    # predicted probabilities: p_i = sigmoid(z_i)
+    # one probability per data point
+    p = sigmoid(z)          # shape (n_samples, )
+
+    # small epsilon to avoid log(0)
+    eps = 1e-9
+
+    # cross-entropy:
+    # J(w) = -1/n * sum [ y_i log(p_i) + (1 - y_i) log(1 - p_i) ]
+    loss = -np.mean(y * np.log(p + eps) + (1 - y) * np.log(1 - p + eps))
+
+    return loss
+
+def logistic_grad(w, X, y):
+    """
+    gradient of the logistic loss function with respect to w (weights).
+    :param w: weight vector, shape (n_features, ).
+    :param X: data matrix, shape (n_samples, n_features).
+    :param y: vector of true labels, shape (n_samples, ).
+    :return: grad: shape (n_features, ).
+    """
+
+    # linear scores
+    z = X @ w               # (n_samples, )
+
+    # predicted probabilities vector
+    p = sigmoid(z)          # (n_samples, )
+
+    # error vector: p_i - y_i
+    error = p - y           # (n_samples, )
+
+    # gradient formula: (1/n) * X^T (p-y)
+    # dividing by len(y) gets the average
+    grad = X.T @ (error / len(y))       # (n_features, )
+
+    return grad
+
+
